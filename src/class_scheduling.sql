f62b411d02a9d6f412a2087c6ea539f3af082b5a
jakeadam10 <jake.adam@ada.ac.uk>
2025-02-18 21:55:55 +0000

    class scheduling and membership management done

--- src/class_scheduling.sql
+++ src/class_scheduling.sql
@@ -25,14 +25,35 @@ VALUES (3, 11, 'Registered'); */
 
 
 -- 4. Cancel a class registration
--- TODO: Write a query to cancel a class registration
-UPDATE class_attendance
-SET attendance_status = 'Deregistered'
-WHERE member_id = 2 AND schedule_id = 7;
+/* UPDATE class_attendance
+SET attendance_status = 'Deregistered' -- Picked appropriate status + Updated schema
+WHERE member_id = 2 AND schedule_id = 7; */
 
 
--- 5. List top 5 most popular classes
--- TODO: Write a query to list top 5 most popular classes
+-- 5. List top 3 most popular classes
+-- TODO: Write a query to list top 3 most popular classes
+
+/*
+SELECT c.class_id, c.name AS class_name, SUM(rc.reg_count) AS reg_count 
+FROM classes c
+JOIN class_schedule cs ON c.class_id = cs.class_id 
+JOIN (
+    SELECT schedule_id, COUNT(*) AS reg_count 
+    FROM class_attendance
+    GROUP BY schedule_id
+    ) rc ON cs.schedule_id = rc.schedule_id 
+GROUP BY c.class_id, c.name 
+ORDER BY reg_count DESC
+LIMIT 3;  
+*/
 
 -- 6. Calculate average number of classes per member
 -- TODO: Write a query to calculate average number of classes per member
+
+SELECT ROUND(AVG(classes_by_mem),2) as Avg_class_per_member_2dp
+FROM (
+    SELECT member_id, COUNT(*) as classes_by_mem
+    FROM class_attendance
+    GROUP BY member_id
+    )
+
--- src/membership_management.sql
+++ src/membership_management.sql
@@ -7,10 +7,27 @@
 -- Membership Management Queries
 
 -- 1. List all active memberships
--- TODO: Write a query to list all active memberships
+/*
+SELECT m.member_id, m.first_name, m.last_name, ms.type AS membership_type, m.join_date
+FROM members m
+JOIN (SELECT * 
+      FROM memberships
+      WHERE status = 'Active') AS ms ON m.member_id = ms.member_id
+
+*/
 
 -- 2. Calculate the average duration of gym visits for each membership type
 -- TODO: Write a query to calculate the average duration of gym visits for each membership type
+SELECT ms.type AS membership_type, 
+       ROUND(AVG(JULIANDAY(a.check_out_time) - JULIANDAY(a.check_in_time)) * 24 * 60, 2) AS avg_visit_duration_minutes
+FROM attendance a
+JOIN memberships ms ON a.member_id = ms.member_id
+GROUP BY ms.type
 
 -- 3. Identify members with expiring memberships this year
--- TODO: Write a query to identify members with expiring memberships this year
+/*
+SELECT m.member_id, m.first_name, m.last_name, m.email, ms.end_date
+FROM members m
+JOIN memberships ms ON m.member_id = ms.member_id
+WHERE ms.end_date BETWEEN DATE('now') AND DATE('now', '+1 year')
+*/
